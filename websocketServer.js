3f0459bd2ad3cb787a002cf7eb0770a565bcb91f
ianlau7 <103270593+ianlau7@users.noreply.github.com>
2024-04-03 18:48:05 -0400

    logsss

--- websocketServer.js
+++ websocketServer.js
@@ -10,6 +10,7 @@ wss.on('connection', function connection(ws) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
+        console.log(message)
       }
     });
   });
