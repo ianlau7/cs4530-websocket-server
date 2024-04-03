563e00b7f731096ad101d6dbe648bec703a7ffb3
ianlau7 <103270593+ianlau7@users.noreply.github.com>
2024-04-03 18:42:12 -0400

    fix?

--- websocketServer.js
+++ websocketServer.js
@@ -6,12 +6,10 @@ wss.on('connection', function connection(ws) {
   console.log('Client connected');
 
   ws.on('message', function incoming(message) {
-    console.log('Received: %s', message);
 
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
-        console.log(message + " sent.")
       }
     });
   });
